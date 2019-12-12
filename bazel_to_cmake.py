57f9b4b98159218209210331ba65d2878df901d7
Florian Berchtold <florian.berchtold@gmail.com>
2019-12-13 00:22:38 +0100

    big overhaul with recursive folder search etc

--- bazel_to_cmake.py
+++ bazel_to_cmake.py
@@ -25,10 +25,25 @@ from __future__ import print_function
 import sys
 import textwrap
 import ast
+import os
+from glob import glob
+from pathlib import Path
 
 
 def StripColons(deps):
     return [item.replace(":", "") for item in deps]
+
+
+def GetTargetName(names):
+    ret_val = []
+    for item in names:
+        if ":" in item:
+            ret_val.append(item[item.rfind(":")+1:])
+        elif "/" in item:
+            ret_val.append(item[item.rfind("/")+1:])
+        else:
+            ret_val.append(item)
+    return ret_val
 
 
 def IsSourceFile(name):
@@ -40,14 +55,30 @@ class BuildFileFunctions(object):
         self.converter = converter
         self.globs = {}  # needed to expand global vars on the fly, i.e. through load()
 
+    def _append_toplevel(self, string):
+        self.converter.append_current_toplevel(string)
+
     def _add_deps(self, kwargs, keyword=""):
         if "deps" not in kwargs:
             return
-        self.converter.toplevel += "target_link_libraries(%s%s\n  %s)\n" % (
+        string = "target_link_libraries(%s%s\n  %s)\n" % (
             kwargs["name"],
             keyword,
-            "\n  ".join(StripColons(kwargs["deps"]))
+            # "\n  ".join(StripColons(kwargs["deps"]))
+            "\n  ".join(GetTargetName(kwargs["deps"]))
         )
+        self._append_toplevel(string)
+
+    def _target_include_directories(self, **kwargs):
+        copts = kwargs.get("copts", [])
+
+        for opt in copts:
+            if opt.startswith("-I"):
+                string = "target_include_directories(%s PUBLIC %s)\n" % (
+                    kwargs["name"],
+                    opt[2:],
+                )
+                self._append_toplevel(string)
 
     def load(self, file, *args):
         file = file.replace("//:", "./")
@@ -59,60 +90,86 @@ class BuildFileFunctions(object):
 
             for var in delta_dir:
                 self.globs[var] = eval(var)
-        pass
 
     def cc_library(self, **kwargs):
         if kwargs["name"] == "amalgamation" or kwargs["name"] == "upbc_generator":
             return
         files = kwargs.get("srcs", []) + kwargs.get("hdrs", [])
 
-        if filter(IsSourceFile, files):
+        if True or filter(IsSourceFile, files):
             # Has sources, make this a normal library.
-            self.converter.toplevel += "add_library(%s\n  %s)\n" % (
+            string = "add_library(%s\n  %s)\n" % (
                 kwargs["name"],
                 "\n  ".join(files)
             )
+            self._append_toplevel(string)
             self._add_deps(kwargs)
+
+            self._target_include_directories(**kwargs)
+
+            string = "SET_TARGET_PROPERTIES(%s PROPERTIES LINKER_LANGUAGE CXX)\n" % kwargs[
+                "name"]
+            self._append_toplevel(string)
+
         else:
             # Header-only library, have to do a couple things differently.
             # For some info, see:
             #  http://mariobadr.com/creating-a-header-only-library-with-cmake.html
-            self.converter.toplevel += "add_library(%s INTERFACE)\n" % (
+            string = "add_library(%s INTERFACE)\n" % (
                 kwargs["name"]
             )
-            self.converter.toplevel += "target_sources(%s \n %s)\n" % (
+            self._append_toplevel(string)
+
+            # TODO: if it is a relative path, its not allowed with target_sources. Maybe with include_...?
+            string = "target_sources(%s INTERFACE\n %s)\n" % (
                 kwargs["name"],
-                "\n  ".join(StripColons(files))
+                # "\n  ".join(StripColons(files))
+                "\n  ".join(files)
             )
+            self._append_toplevel(string)
             self._add_deps(kwargs, " INTERFACE")
 
     def cc_binary(self, **kwargs):
-        pass
+        files = kwargs.get("srcs", []) + kwargs.get("hdrs", [])
+
+        string = "add_executable(%s\n  %s)\n" % (
+            kwargs["name"],
+            "\n  ".join(files)
+        )
+        self._append_toplevel(string)
+        self._add_deps(kwargs)
+
+        self._target_include_directories(**kwargs)
 
     def cc_test(self, **kwargs):
         # Disable this until we properly support upb_proto_library().
-        # self.converter.toplevel += "add_executable(%s\n  %s)\n" % (
-        #     kwargs["name"],
-        #     "\n  ".join(kwargs["srcs"])
-        # )
-        # self.converter.toplevel += "add_test(NAME %s COMMAND %s)\n" % (
-        #     kwargs["name"],
-        #     kwargs["name"],
-        # )
+        string = "add_executable(%s\n  %s)\n" % (
+            kwargs["name"],
+            "\n  ".join(kwargs["srcs"])
+        )
+        self._append_toplevel(string)
 
-        # if "data" in kwargs:
-        #   for data_dep in kwargs["data"]:
-        #     self.converter.toplevel += textwrap.dedent("""\
-        #       add_custom_command(
-        #           TARGET %s POST_BUILD
-        #           COMMAND ${CMAKE_COMMAND} -E copy
-        #                   ${CMAKE_SOURCE_DIR}/%s
-        #                   ${CMAKE_CURRENT_BINARY_DIR}/%s)\n""" % (
-        #       kwargs["name"], data_dep, data_dep
-        #     ))
+        string = "add_test(NAME %s COMMAND %s)\n" % (
+            kwargs["name"],
+            kwargs["name"],
+        )
+        self._append_toplevel(string)
 
-        # self._add_deps(kwargs)
-        pass
+        if "data" in kwargs:
+            for data_dep in kwargs["data"]:
+                string = textwrap.dedent("""\
+              add_custom_command(
+                  TARGET %s POST_BUILD
+                  COMMAND ${CMAKE_COMMAND} -E copy
+                          ${CMAKE_SOURCE_DIR}/%s
+                          ${CMAKE_CURRENT_BINARY_DIR}/%s)\n""" % (
+                    kwargs["name"], data_dep, data_dep
+                ))
+                self._append_toplevel(string)
+
+        self._add_deps(kwargs)
+
+        self._target_include_directories(**kwargs)
 
     def py_library(self, **kwargs):
         pass
@@ -174,6 +231,9 @@ class BuildFileFunctions(object):
     def map_dep(self, arg):
         return arg
 
+    def package(self, **kwargs):
+        pass
+
 
 class WorkspaceFileFunctions(object):
     def __init__(self, converter):
@@ -183,7 +243,7 @@ class WorkspaceFileFunctions(object):
         pass
 
     def workspace(self, **kwargs):
-        self.converter.prelude += "project(%s)\n" % (kwargs["name"])
+        self.converter.prelude += "project(%s C CXX)\n" % (kwargs["name"])
 
     def http_archive(self, **kwargs):
         pass
@@ -198,16 +258,35 @@ class WorkspaceFileFunctions(object):
 class Converter(object):
     def __init__(self):
         self.prelude = ""
-        self.toplevel = ""
+        self.toplevel = {}
+        self.include_subdirs = {}
         self.if_lua = ""
+        self.current_build_file = ""
 
-    def convert(self):
-        return self.template % {
-            "prelude": converter.prelude,
-            "toplevel": converter.toplevel,
-        }
+    def append_current_toplevel(self, string):
+        if self.current_build_file not in self.toplevel:
+            self.toplevel[self.current_build_file] = ""
 
-    template = textwrap.dedent("""\
+        self.toplevel[self.current_build_file] = self.toplevel[self.current_build_file] + string
+
+    def append_current_subdir(self, file):
+        if self.current_build_file not in self.include_subdirs:
+            self.include_subdirs[self.current_build_file] = []
+
+        self.include_subdirs[self.current_build_file].append(file)
+
+    def convert_prelude(self):
+        return self.prelude_template % {"prelude": self.prelude}
+
+    def convert_toplevel(self):
+        include_dirs = ""
+        if self.current_build_file in self.include_subdirs:
+            for directory in self.include_subdirs[self.current_build_file]:
+                include_dirs += "include_directories(%s)\n" % directory.replace(
+                    ".", "").replace("/", "")
+        return "%s\n%s" % (include_dirs, self.toplevel[self.current_build_file])
+
+    prelude_template = textwrap.dedent("""\
     # This file was generated from BUILD using tools/make_cmakelists.py.
 
     cmake_minimum_required(VERSION 3.1)
@@ -222,6 +301,8 @@ class Converter(object):
     cmake_policy(SET CMP0048 NEW)
 
     %(prelude)s
+
+    SET(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=c++17")
 
     # Prevent CMake from setting -rdynamic on Linux (!!).
     SET(CMAKE_SHARED_LIBRARY_LINK_C_FLAGS "")
@@ -257,8 +338,8 @@ class Converter(object):
       set(CMAKE_SHARED_LINKER_FLAGS "${CMAKE_SHARED_LINKER_FLAGS} -fsanitize=address")
     endif()
 
-    include_directories(.)
-    include_directories(${CMAKE_CURRENT_BINARY_DIR})
+    #include_directories(.)
+    #include_directories(${CMAKE_CURRENT_BINARY_DIR})
 
     if(APPLE)
       set(CMAKE_SHARED_LINKER_FLAGS "${CMAKE_SHARED_LINKER_FLAGS} -undefined dynamic_lookup -flat_namespace")
@@ -268,9 +349,11 @@ class Converter(object):
 
     enable_testing()
 
-    %(toplevel)s
 
-  """)
+    """)
+
+    def convert(self):
+        return self.convert_prelude() + self.convert_toplevel()
 
 
 data = {}
@@ -288,15 +371,46 @@ def GetDict(obj):
     return ret
 
 
+def ParseBuildFile(path, converter):
+    cmake_created = False
+
+    for directory in glob(path+"*/"):
+        if not os.path.islink(Path(directory)):
+            cmake_created_in_dir = ParseBuildFile(directory, converter)
+            if cmake_created_in_dir:
+                cmake_created = True
+                converter.current_build_file = os.path.join(path, "BUILD")
+                converter.append_current_subdir(directory)
+
+    if "BUILD" in os.listdir(path):
+        file = os.path.join(path, "BUILD")
+        converter.current_build_file = file
+        code_block = compile(open(file).read(), file, 'exec')
+        build = BuildFileFunctions(converter)
+        code_globals = GetDict(build)
+        build.globs = code_globals
+        exec(code_block, code_globals)
+        return True
+
+    return cmake_created
+
+
 globs = GetDict(converter)
 
 exec(open("WORKSPACE").read(), GetDict(WorkspaceFileFunctions(converter)))
 
-code_block = compile(open('BUILD').read(), 'BUILD', 'exec')
-build = BuildFileFunctions(converter)
-code_globals = GetDict(build)
-build.globs = code_globals
-exec(code_block, code_globals)
+ParseBuildFile("./", converter)
 
-with open(sys.argv[1], "w") as f:
-    f.write(converter.convert())
+to_create_files = set(converter.include_subdirs.keys())
+to_create_files.update(set(converter.toplevel.keys()))
+
+for affected_subdirs in to_create_files:
+    with open(affected_subdirs.replace("BUILD", sys.argv[1]), "w") as f:
+        converter.current_build_file = affected_subdirs
+        if "./BUILD" == affected_subdirs:
+            f.write(converter.convert())
+        else:
+            f.write(converter.convert_toplevel())
+
+# with open(sys.argv[1], "w") as f:
+#    f.write(converter.convert())
